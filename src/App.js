a64caebbf8693e256325dfe5c67070739b489171
lNarcizo <102670281+lNarcizo@users.noreply.github.com>
2022-03-30 17:39:59 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {React} from 'react';
+import HelloWorld from './components/HelloWorld.jsx';
+import Pessoa from './components/Pessoa.jsx';
+
 
 function App() {
+
+  const nome2 = "Elifas Brabo";
+
+  const idade = 21;
+
+  const profissao = "Scrum master AVA WorldPress"
+
+  const nome = "Marcos Trouxa";
+
+  let num1 = 5;
+
+  let num2 = 3;
+
+  const url = "https://i.pinimg.com/originals/03/53/dc/0353dc2ac4ab11998506e27066c6ba51.jpg";
+
+  function sum(a,b) { return a+b }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá {nome}</h1>
+      <img src={url} className='App-logo' alt='Teste'/>
+      <p>A soma entre {num1} e {num2} é: {sum(num1,num2)}</p>
+      <HelloWorld/>
+      <Pessoa nome={nome2} idade={idade} profissao={profissao}/>
     </div>
   );
 }
